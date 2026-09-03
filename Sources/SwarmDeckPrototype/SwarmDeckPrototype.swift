4f9c93dfdff992eb0f3db72d6b95627bf7298780
Rafael Klein Scharf <42837920+RafaelScharf@users.noreply.github.com>
2026-09-03 18:33:13 -0300

    Force window key and activation for input

--- Sources/SwarmDeckPrototype/SwarmDeckPrototype.swift
+++ Sources/SwarmDeckPrototype/SwarmDeckPrototype.swift
@@ -3,9 +3,21 @@ import GhosttyTerminal
 
 @main
 struct SwarmDeckPrototypeApp: App {
+    @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+        }
+    }
+}
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        NSApplication.shared.setActivationPolicy(.regular)
+        NSApplication.shared.activate(ignoringOtherApps: true)
+        if let window = NSApplication.shared.windows.first {
+            window.makeKeyAndOrderFront(nil)
         }
     }
 }
